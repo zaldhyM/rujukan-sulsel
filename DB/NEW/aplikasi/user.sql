4ef05ecba84571011c8a9a19563c6e5b8a60b972
zaldhy <zaldhymasud@gmail.com>
2025-08-13 15:22:35 +0800

    Perbaikan script insert user

--- DB/NEW/aplikasi/user.sql
+++ DB/NEW/aplikasi/user.sql
@@ -9,7 +9,7 @@ CREATE TABLE `user` (
   `NIK` char(16) DEFAULT NULL,
   `ID_FASKES` smallint DEFAULT '0',
   `MASA_AKTIF` date DEFAULT NULL,
-  `TOKEN` text DEFAULT '',
+  `TOKEN` text NOT NULL DEFAULT '',
   `ROLE` enum('admin', 'faskes', 'dinkeskab', 'dinkesprov') COMMENT 'penentuan alur hak akses berdasarkan role',
   `STATUS_TELEGRAM` tinyint DEFAULT '0' COMMENT '1. telgram aktif 0. tidak aktif',
   `ID_TELEGRAM` char(12) DEFAULT '' COMMENT 'menyimpan id telegram dari user',
