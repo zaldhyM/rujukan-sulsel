8058c0be3a3700c4a9d178eaaa34745b3059f9cf
zaldhy <zaldhymasud@gmail.com>
2025-08-20 17:23:04 +0800

    Perbaikan init sql

--- DB/init.sql
+++ DB/init.sql
@@ -1,5 +1,5 @@
 -- Buat user 'admin' (jika belum otomatis dibuat)
-CRCREATE USER 'admin'@'%' IDENTIFIED WITH caching_sha2_password BY 'S!MGos2@kemkes.go.id';
+CRCREATE USER 'admin'@'%' IDENTIFIED WITH caching_sha2_password BY 'S!GApRujukan@kemkes.go.id';
 
 -- Berikan akses penuh ke semua database (termasuk mysql.*, performance_schema.*, sys.*, test, dll)
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
