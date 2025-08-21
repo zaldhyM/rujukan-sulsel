b576f05b84be354ff1c8180cff053345a6b9ceb8
zaldhy <zaldhymasud@gmail.com>
2025-08-21 22:33:14 +0800

    Perbaikan workspacce

--- apps/sigap/resources/views/workspace.blade.php
+++ apps/sigap/resources/views/workspace.blade.php
@@ -1,8 +1,1 @@
 @extends('layouts.workspace')
-
-@section('title', 'Halaman Index')
-
-@section('content')
-    <h2>Selamat Datang di Halaman Index</h2>
-    <p>Ini adalah main content.</p>
-@endsection
